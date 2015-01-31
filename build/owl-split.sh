d65aeb569aef7ef61fb6c8a688dd603997aec0e4
Scott Bedard <scottbedard90@gmail.com>
2015-01-31 18:09:12 -0500

    Build scripts

--- build/owl-split.sh
+++ build/owl-split.sh
@@ -4,7 +4,4 @@
 
 ./git-subsplit.sh init git@github.com:october-widgets/library.git
 ./git-subsplit.sh publish --no-tags src/FormWidgets/Comment:git@github.com:october-owl/comment.git
-./git-subsplit.sh publish --no-tags src/FormWidgets/GeoAddress:git@github.com:october-owl/geoaddress.git
-./git-subsplit.sh publish --no-tags src/FormWidgets/Hasmany:git@github.com:october-owl/hasmany.git
-./git-subsplit.sh publish --no-tags src/FormWidgets/Tagbox:git@github.com:october-owl/tagbox.git
 rm -rf .subsplit/
